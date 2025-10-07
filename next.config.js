71d4baaacd8938b60a3a58d3660b74f1051b93d8
Joshwa23 <146504396+Joshwa23@users.noreply.github.com>
2025-10-07 15:39:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This makes Next generate a static site in the `out/` folder after `npm run build`
-  output: 'export',
-};
 
-module.exports = nextConfig;
