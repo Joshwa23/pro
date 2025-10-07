17c65d307ddcde1af9d8f4302d0f390b81cdd864
Joshwa23 <146504396+Joshwa23@users.noreply.github.com>
2025-10-07 15:37:30 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,14 +1,1 @@
-export type Job = {
-  id: string;
-  title: string;
-  company: string;
-  location: string;
-  type: "Full-time" | "Part-time" | "Contract" | "Internship";
-  level: "Entry" | "Mid-level" | "Senior";
-  remote: boolean;
-  postedAgo: string;
-  salaryMin: number;
-  salaryMax: number;
-  tags: string[];
-  description: string;
-};
+
